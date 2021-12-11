b55e57c16073ce3a6f11b82fdcaa5e7b01567445
SikorskiSebastian <sebastian.sikorski0702@gmail.com>
2021-12-11 17:31:12 +0100

    now dane, refaktoryzacja
    1) dodanie nowych plików z danymi oraz informacji o nich w folderze dane tzn.: c1 c2 c3 c4 c_info
    
    2) poprawa czytelności kodu

--- src/backsubst.c
+++ src/backsubst.c
@@ -5,17 +5,13 @@
  * Zwraca 2 - błąd nieprawidłowych rozmiarów macierzy
  */
 int backsubst(Matrix *x, Matrix *mat, Matrix *b) {
-				/**
-				 * Tutaj należy umieścić właściwą implemntację.
-				 */
+	/**
+	* Tutaj należy umieścić właściwą implemntację.
+	*/
 
-				/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
-
-				for (int i =0; i < x->r; i++) {
-								x->data[i][0] = b->data[i][0];
-				}
-
-				return 0;
+	/* To ponizej jest przepisaniem b do x. Nalezy to poprawic! */
+	for (int i =0; i < x->r; i++) {
+		x->data[i][0] = b->data[i][0];
+	}
+	return 0;
 }
-
-
